ed8b4978f6ff53015d8959d83b24ffba00e56303
eya gueddes <eyagueddes1@gmail.com>
2021-09-17 21:08:31 +0100

    login with validation

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:untitled1/shared/components/components.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   var emailController = TextEditingController();
+
   var passwordController = TextEditingController();
+
   final String assetName = 'assets/images/login.svg';
 
+  var _formKey = GlobalKey<FormState>();
+
+  bool isPasswordShow=true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,96 +27,89 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
-
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: SvgPicture.asset(
-                assetName,
-                ),
-              ),
-              Text(
-                'Login',
-                textAlign: TextAlign.start,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onFieldSubmitted: (String value) {
-                  print(value);
-                },
-                decoration: InputDecoration(
-                  labelText: 'Email Address',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.email,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  child: SvgPicture.asset(
+                    assetName,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                  ),
-                  prefixIcon: Icon(
-                    Icons.lock,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.remove_red_eye,
-                    color: Colors.grey,
-                  ),
+                Text(
+                  'Login',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: double.infinity,
-                color: Colors.blueGrey[400],
-                child: MaterialButton(
-                  onPressed: () {
-                    print(emailController.text);
-                    print(passwordController.text);
+                SizedBox(
+                  height: 10,
+                ),
+                defaultFormField(
+                  controller: emailController,
+                  type: TextInputType.emailAddress,
+                  label: 'Email',
+                  prefix: Icons.email,
+                  validate: (String value) {
+                    if (value.isEmpty) {
+                      return 'email address is empty';
+                    }
+                    return null;
                   },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Dont\'t have an account?'),
-                  TextButton(onPressed: (){},
-                    child: Text('Register Now'),
-                  )
-                ],
-              ),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                defaultFormField(
+                  controller: passwordController,
+                  type: TextInputType.visiblePassword,
+                  isPassword: isPasswordShow,
+                  label: 'Password',
+                  prefix: Icons.lock,
+                  suffix: isPasswordShow ? Icons.visibility :Icons.visibility_off,
+                  validate: (String value) {
+                    if (value.isEmpty) {
+                      return 'password is empty';
+                    }
+                    return null;
+                  },
+                  suffixPressed: (){
+                    setState(() {
+                     isPasswordShow=!isPasswordShow;
+                    });
+                  }
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                defaultButton(
+                    background: Colors.blueGrey[400],
+                    function: () {
+                      if (_formKey.currentState.validate()) {
+                        print(emailController.text);
+                        print(passwordController.text);
+                      }
+                    },
+                    //
+                    text: 'Login'),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Dont\'t have an account?'),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Register Now'),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
