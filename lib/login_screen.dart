6487e7d49feeb8f90ed213cf7b51057bdf9a2e88
eya gueddes <eyagueddes1@gmail.com>
2021-11-02 01:53:23 +0100

    first commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:untitled1/classes/language.dart';
 import 'package:untitled1/shared/components/components.dart';
+import "package:translator/translator.dart";
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -19,100 +21,134 @@ class _LoginScreenState extends State<LoginScreen> {
 
   bool isPasswordShow=true;
 
+  final translator = GoogleTranslator();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey[400],
+        actions: <Widget>[
+         Padding(padding: EdgeInsets.only(right: 15.0),
+         child: DropdownButton(
+           onChanged: (Language language)async{
+
+           },
+           icon: Icon(Icons.language,
+           color:Colors.white),
+           items:Language.languageList().map<DropdownMenuItem<Language>>((lang) => DropdownMenuItem(
+             value:lang,
+             child: Row(
+               children: <Widget>[
+                 Text(lang.flag,style: TextStyle(fontSize: 8)),
+                 SizedBox(
+                   width: 5
+                 ),
+                 Text(lang.name,style: TextStyle(fontSize: 13),)
+               ],
+             ),
+           )).toList(),
+
+         ),
+
+         )
+
+        ],
+
+
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  child: SvgPicture.asset(
-                    assetName,
+      body: Center(
+        child: Container(
+          width:MediaQuery.of(context).size.width*0.7,
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
                   ),
-                ),
-                Text(
-                  'Login',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                defaultFormField(
-                  controller: emailController,
-                  type: TextInputType.emailAddress,
-                  label: 'Email',
-                  prefix: Icons.email,
-                  validate: (String value) {
-                    if (value.isEmpty) {
-                      return 'email address is empty';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                defaultFormField(
-                  controller: passwordController,
-                  type: TextInputType.visiblePassword,
-                  isPassword: isPasswordShow,
-                  label: 'Password',
-                  prefix: Icons.lock,
-                  suffix: isPasswordShow ? Icons.visibility :Icons.visibility_off,
-                  validate: (String value) {
-                    if (value.isEmpty) {
-                      return 'password is empty';
-                    }
-                    return null;
-                  },
-                  suffixPressed: (){
-                    setState(() {
-                     isPasswordShow=!isPasswordShow;
-                    });
-                  }
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                defaultButton(
-                    background: Colors.blueGrey[400],
-                    function: () {
-                      if (_formKey.currentState.validate()) {
-                        print(emailController.text);
-                        print(passwordController.text);
+
+                  Container(
+                    height:MediaQuery.of(context).size.height*0.3,
+                    width:MediaQuery.of(context).size.width*0.6,
+                    child: SvgPicture.asset(
+                      assetName,
+                    ),
+                  ),
+                  Text(
+                    'Login',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  defaultFormField(
+                    controller: emailController,
+                    type: TextInputType.emailAddress,
+                    label: 'User identifier',
+                    prefix: Icons.email,
+                    validate: (String value) {
+                      if (value.isEmpty) {
+                        return 'user id is empty';
                       }
+                      return null;
                     },
-                    //
-                    text: 'Login'),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Dont\'t have an account?'),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text('Register Now'),
-                    )
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  defaultFormField(
+                    controller: passwordController,
+                    type: TextInputType.visiblePassword,
+                    isPassword: isPasswordShow,
+                    label: 'Password',
+                    prefix: Icons.lock,
+                    suffix: isPasswordShow ? Icons.visibility :Icons.visibility_off,
+                    validate: (String value) {
+                      if (value.isEmpty) {
+                        return 'password is empty';
+                      }
+                      return null;
+                    },
+                    suffixPressed: (){
+                      setState(() {
+                       isPasswordShow=!isPasswordShow;
+                      });
+                    }
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  defaultButton(
+                      background: Colors.blueGrey[400],
+                      function: () {
+                        if (_formKey.currentState.validate()) {
+                          print(emailController.text);
+                          print(passwordController.text);
+                        }
+                      },
+                      //
+                      text: 'Login'),
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _changeLanguage(Language language) {
+    print(language.languageCode);
+
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "DRAX`com",
       debugShowCheckedModeBanner: false,
       home:LoginScreen(),
     );
